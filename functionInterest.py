a6c5ec33d7a4963a0fa9064e4c3c572b22dc85cb
Dhanasekar K <70059744+Dhanaskv@users.noreply.github.com>
2020-10-23 07:35:01 +0530

    Delete functionInterest.py

--- functionInterest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-def simple_interest(p,t,r):
-    return (p*t*r)/100
-p=float(input("The principal amount is:"))
-t=float(input("The Rate of interest is:"))
-r=float(input("The Time of years is:"))
-print("The simple interest is:",simple_interest(p,t,r))
-
