c8a299d2225e75eefd8befcdd965c6432c4312ba
Dhanasekar K <70059744+Dhanaskv@users.noreply.github.com>
2020-10-23 10:18:34 +0530

    basic tuple programs

---
+++ TupleLoop.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+T1=(1,2,3,4,5,6,7,8,9,10,11,12,"python")
+n=input("User input value is: ")
+for i in T1:
+    if (n == i):
+        print("The value is equal")
---
+++ tuples.py
@@ -0,0 +1,3 @@
+Tuple1=(1,2,"python",4.5,"script")
+print(type(Tuple1))
+print(Tuple1)
