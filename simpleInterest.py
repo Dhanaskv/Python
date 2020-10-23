e52729b91c6f186053b367b1edf019e86c6d44fd
Dhanasekar K <70059744+Dhanaskv@users.noreply.github.com>
2020-10-23 07:31:20 +0530

    Delete simpleInterest.py

--- simpleInterest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-def functioNname(prinAmount,rate,time):
-    return (prinAmount*rate*time)/100
-
-p=int(input("The principle amount is :"))
-r=float(input("The rate is interest is :"))        
-t=float(input("The time of periods is :"))
-print("The simple interest is : ",functioNname(p,r,t))    
