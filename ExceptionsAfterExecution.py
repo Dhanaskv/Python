2dcb6e87a96cc6613810aa6c43501fbf495d8cba
Dhanasekar K <70059744+Dhanaskv@users.noreply.github.com>
2020-10-31 09:19:51 +0530

    basic python programming

---
+++ ExceptionsAfterExecution.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+try:
+    a=input("first value is: ")
+    b=input("second value is: ")
+    c=a/b
+    print(c)
+except:
+    print("can't divide")
+else:
+    print("The else statement is executed")
+
+var=(1,2,3,4,5,6)
+for i in var:
+    print(i)
