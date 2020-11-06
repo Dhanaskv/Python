8b70b1cfe3047d238a0c223afd515c2993439c89
Dhanasekar K <70059744+Dhanaskv@users.noreply.github.com>
2020-11-06 17:47:49 +0530

    List Basic Problems

---
+++ WaysToFindLengthOfList.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+L1=[10,20,30,40,50,60,20,39,10,60,80,70,100]
+count=0
+for i in L1:
+    count=count+1
+print("The total count is: ",count)
+print(" OR Another method we are using")
+print(len(L1))
