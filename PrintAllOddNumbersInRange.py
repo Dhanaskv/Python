60aa28c955ad183dd04f2c243e4e0a60129f283a
Dhanasekar K <70059744+Dhanaskv@users.noreply.github.com>
2020-11-07 08:45:32 +0530

    basic List programs

---
+++ PrintAllOddNumbersInRange.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+start=4
+end=16
+for i in range(start,end+1):
+    if i%2 != 0:
+        print("The Odd range value is: ",i)
---
+++ waysToFindPositiveNumbersInList.py
@@ -0,0 +1,5 @@
+#1/usr/bin/python
+L1=[-1,12,-2,4,-5,8,0]
+for i in L1:
+    if i>=0:
+        print("The positive value is:",i)
