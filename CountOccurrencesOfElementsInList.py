62236cc5a6dc541e3e678d42661b12845e202472
Dhanasekar K <70059744+Dhanaskv@users.noreply.github.com>
2020-11-07 10:24:10 +0530

    basic list programs

---
+++ CountOccurrencesOfElementsInList.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+L1=[10,20,10,30,50,10,10,10,10,10,10,10,10,10,10]
+x=10
+count=0
+for i in L1:
+    if i == x:
+        count=count+1
+print("the count occurance is : ",count)
+        
